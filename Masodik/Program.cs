4a431be7dce6f2d3230cf7cc2ec3203b71357de8
BabakAdam <babaka@kkszki.hu>
2024-10-18 10:27:04 +0200

    :3

--- Masodik/Program.cs
+++ Masodik/Program.cs
@@ -10,6 +10,57 @@ namespace Masodik
     {
         static void Main(string[] args)
         {
+                string jelszo;
+
+                while (true)
+                {
+                    
+                    Console.Write("Kérem, adja meg a jelszót: ");
+                    jelszo = Console.ReadLine();
+
+                    
+                    if (IsValidPassword(jelszo))
+                    {
+                        Console.WriteLine("Jelszó elfogadva!");
+                        break; 
+                    }
+                    else
+                    {
+                        Console.WriteLine("A jelszó nem felel meg a következő kritériumoknak:");
+                        Console.WriteLine("- Legalább 8 karakter hosszú");
+                        Console.WriteLine("- Tartalmaz legalább egy számot");
+                        Console.WriteLine("- Tartalmaz legalább egy nagybetűt");
+                    }
+                }
+            }
+
+            
+            static bool IsValidPassword(string password)
+            {
+                
+                if (password.Length < 8)
+                {
+                    return false;
+                }
+
+                
+                bool containsNumber = false;
+                bool containsUpperCase = false;
+
+                foreach (char c in password)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        containsNumber = true;
+                    }
+                    if (char.IsUpper(c))
+                    {
+                        containsUpperCase = true;
+                    }
+                }
+
+                return containsNumber && containsUpperCase;
+            }
         }
+
     }
-}
